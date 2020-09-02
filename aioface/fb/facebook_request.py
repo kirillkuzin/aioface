c5ed179ecd407ab1963df5ff1d0564456982d162
Kirill Kuzin <offkirillkuzin@gmail.com>
2020-09-02 07:21:39 +0300

    Added list template support

--- aioface/fb/facebook_request.py
+++ aioface/fb/facebook_request.py
@@ -9,10 +9,13 @@ import aiohttp
 
 @dataclass
 class FacebookButton:
-    title: str
     type: str
+    title: str = None
     payload: str = None
     url: str = None
+    messenger_extensions: bool = None
+    webview_height_ratio: str = None
+    fallback_url: str = None
 
 
 @dataclass
@@ -20,6 +23,7 @@ class FacebookTemplate:
     title: str
     subtitle: str = None
     image_url: str = None
+    default_action: FacebookButton = None
     buttons: typing.List[FacebookButton] = None
 
 
@@ -34,9 +38,11 @@ class FacebookQuickReply:
 @dataclass
 class FacebookAttachmentPayload:
     template_type: str = None
+    top_element_style: str = None
     elements: typing.List[FacebookTemplate] = None
+    buttons: typing.List[FacebookButton] = None
     url: str = None
-    is_reusable: bool = False
+    is_reusable: bool = None
 
 
 @dataclass
