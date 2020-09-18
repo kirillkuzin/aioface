7e255a66de7ee42abc702ea3137e50a98aa4bf40
Kirill Kuzin <offkirillkuzin@gmail.com>
2020-09-18 17:30:28 +0300

    Removed unnecessary methods

--- aioface/storages/base_storage.py
+++ aioface/storages/base_storage.py
@@ -2,14 +2,6 @@ import typing
 
 
 class BaseStorage:
-    async def close(self) -> None:
-        raise NotImplementedError
-
-    @classmethod
-    def check_user_id(cls, user_id: str) -> str:
-        if user_id is None:
-            raise ValueError('user_id parameter must be provided')
-        return user_id
 
     async def get_data(self, user_id: str) -> typing.Dict:
         raise NotImplementedError
