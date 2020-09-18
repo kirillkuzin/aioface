7bc2e55aa42652cfe6e93524c850c341831ea3dc
Kirill Kuzin <offkirillkuzin@gmail.com>
2020-09-18 17:38:14 +0300

    Added ABC

--- aioface/storages/base_storage.py
+++ aioface/storages/base_storage.py
@@ -1,15 +1,20 @@
 import typing
+from abc import ABC, abstractmethod
 
 
-class BaseStorage:
+class BaseStorage(ABC):
+    @abstractmethod
     async def get_data(self, user_id: str) -> typing.Dict:
         raise NotImplementedError
 
+    @abstractmethod
     async def set_data(self, user_id: str, data: typing.Dict):
         raise NotImplementedError
 
+    @abstractmethod
     async def update_data(self, user_id: str, data: typing.Dict):
         raise NotImplementedError
 
+    @abstractmethod
     async def reset_data(self, user_id: str):
         await self.set_data(user_id=user_id, data=dict())
