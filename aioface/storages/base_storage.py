fc19622d96ade6adffb6b26a0a469c95d08f0210
Kirill Kuzin <offkirillkuzin@gmail.com>
2020-09-18 17:38:36 +0300

    Removed async

--- aioface/storages/base_storage.py
+++ aioface/storages/base_storage.py
@@ -4,17 +4,17 @@ from abc import ABC, abstractmethod
 
 class BaseStorage(ABC):
     @abstractmethod
-    async def get_data(self, user_id: str) -> typing.Dict:
+    def get_data(self, user_id: str) -> typing.Dict:
         raise NotImplementedError
 
     @abstractmethod
-    async def set_data(self, user_id: str, data: typing.Dict):
+    def set_data(self, user_id: str, data: typing.Dict):
         raise NotImplementedError
 
     @abstractmethod
-    async def update_data(self, user_id: str, data: typing.Dict):
+    def update_data(self, user_id: str, data: typing.Dict):
         raise NotImplementedError
 
     @abstractmethod
-    async def reset_data(self, user_id: str):
+    def reset_data(self, user_id: str):
         await self.set_data(user_id=user_id, data=dict())
